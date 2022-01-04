fe30008b534b31c16bd76a3d72feeaf1c43ba7df
zwonlala <s26788761@naver.com>
2022-01-04 20:43:09 +0900

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+
+const Circle = styled.div`
+    width: 5rem;
+    height: 5rem;
+    background: black;
+    border-radius: 50%;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <Circle/>;
 }
 
 export default App;
