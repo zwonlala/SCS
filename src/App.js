80cf79cc6c085afe3babfbc59f112af6d9481c02
zwonlala <s26788761@naver.com>
2022-01-04 20:55:12 +0900

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 const Circle = styled.div`
     width: 5rem;
     height: 5rem;
     background: ${props => props.color || 'black'};
     border-radius: 50%;
+    ${props => 
+        props.huge && 
+        css`
+            width: 10rem;
+            height: 10rem;
+        `}
 `;
 
 function App() {
-    return <Circle color="blue"/>;
+    return <Circle color="blue" huge/>;
 }
 
 export default App;
