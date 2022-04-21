a1e426d5d4df2594b5fbfc5ad1af5cf0361e2777
zwonlala <s26788761@naver.com>
2022-04-21 23:28:42 +0900

    9th commit
    색상 관련 된 코드만 따로 분리하여, 해당 코드 사용하는 구조로 변경

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,21 @@ import React from 'react';
 import styled, {css} from 'styled-components';
 import {darken, lighten} from 'polished';
 
+const colorStyles = css`
+	${({ theme, color }) => {
+		const selected = theme.palette[color];
+		return css`
+			background: ${selected};
+			&:hover {
+				background: ${lighten(0.1, selected)};
+			}
+			&:active {
+				background: ${darken(0.1, selected)};
+			}
+		`;
+	}}
+`;
+
 const StyledButton = styled.button`
 	display: inline-block;
 	outline: none;
@@ -18,27 +33,15 @@ const StyledButton = styled.button`
 	font-size: 1rem;
 
 	// color
-	${props => {
-		const selected = props.theme.palette[props.color];
-
-		return css`
-			background: ${selected};
-			&:hover {
-				background: ${lighten(0.1, selected)};
-			}
-			&:active {
-				background: ${darken(0.1, selected)};
-			}
-		`
-	}}
+	${colorStyles}
 
 	& + & {
 		margin-left: 1rem;
 	}
 `;
 
-function Button({children, ...rest}) {
-    return <StyledButton {...rest}>{children}</StyledButton>;
+function Button({children, color, ...rest}) {
+    return <StyledButton color={color} {...rest}>{children}</StyledButton>;
 }
 
 Button.defaultProps = {
