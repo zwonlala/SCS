a986e9f4f3c75cec476042e03d10daf655866a49
zwonlala <s26788761@naver.com>
2022-04-24 20:20:28 +0900

    15th commit
    <Dialog /> 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled, {ThemeProvider} from 'styled-components';
 import Button from './components/Button';
+import Dialog from './components/Dialog';
 
 const AppBlock = styled.div`
     width: 512px;
@@ -57,8 +58,15 @@ function App() {
 					<Button size="large" color="gray" fullWidth>BUTTON</Button>
 					<Button size="large" color="pink" fullWidth>BUTTON</Button>
 				</ButtonGroup>
-
 			</AppBlock>
+
+			<Dialog
+				title='정말로 삭제하시겠습니까?'
+				confirmText='삭제'
+				cancelText='취소'
+			>
+				데이터를 정말로 삭제하시겠습니까?
+			</Dialog>
 		</ThemeProvider>
 	);
 }
